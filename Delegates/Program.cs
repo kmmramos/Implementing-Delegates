151b64baa58e8a572aa03b36fdb891b34b671178
kirstinramos <kirstinramos@merchantsfleet.com>
2022-08-06 17:23:09 +0800

    Initial commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Delegates
+{
+    public class Program
+    {
+        static void Main(string[] path)
+        {
+            var processor = new PhotoProcessor();
+            var filter = new PhotoFilters();
+
+            //PhotoProcessor.PhotoFilterHandler filterHandler = filter.ApplyBrightness;
+            Action<Photo> filterHandler = filter.ApplyBrightness;
+            filterHandler += filter.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+
+    }
+}
